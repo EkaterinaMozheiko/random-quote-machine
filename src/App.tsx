03fb7abef2a766494773c3ad205d9bb011dc31cb
Ekaterina Mozheiko <ek.mozheiko@gmail.com>
2019-02-03 19:35:46 +0700

    add 'show new quote' method

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import './App.scss';
 
 interface componentState {
-  quoteText: string,
-  quoteAuthor: string
+  quoteText: string;
+  quoteAuthor: string;
 }
 
 class App extends Component<{}, componentState> {
@@ -14,19 +17,38 @@ class App extends Component<{}, componentState> {
     };
   }
 
+  componentDidMount() {
+    this.getNewQuote();
+  }
+
+  handleClick = () => {
+    this.getNewQuote();
+  }
+
+  getNewQuote() {
+    axios.get(
+      'https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&json=?'
+    ).then(response => 
+      this.setState({
+        quoteText: response.data.quoteText,
+        quoteAuthor: response.data.quoteAuthor
+      })
+    );
+  };
+
   render() {
+    const { quoteText, quoteAuthor } = this.state;
     return (
-      <div id="quote-box">
-        <div id="text">
-        TEXT
+      <div className="wrapper">
+        <div id="quote-box" className="quote-box">
+          <div id="text">{quoteText}</div>
+          <div id="author">{quoteAuthor}</div>
+          <button id="new-quote" onClick={this.handleClick}>
+            NEW QUOTE
+          </button>
+          <a id="tweet-quote">TWEET QUOTE</a>
         </div>
-        <div id="author">
-        AUTHOR
-        </div>
-        <button id="new-quote">NEW QUOTE</button>
-        <button id="tweet-quote">TWEET QUOTE</button>
       </div>
-      
     );
   }
 }
