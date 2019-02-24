9964915399c9e452360fc81b224a48f7d8dc8976
Ekaterina Mozheiko <ek.mozheiko@gmail.com>
2019-02-24 14:32:08 +0700

    add sharing to twitter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 import './App.scss';
 
@@ -7,7 +7,18 @@ interface componentState {
   quoteText: string;
   quoteAuthor: string;
   currentColor: string;
+  quoteLink: string;
 }
+
+interface QuoteContent {
+  quoteAuthor: string;
+  quoteLink: string;
+  quoteText: string;
+  senderLink: string;
+  senderName: string;
+}
+
+const errorMessage = 'Sorry! There is no quote:( Try again';
 
 class App extends Component<{}, componentState> {
   constructor(props: any) {
@@ -16,7 +27,8 @@ class App extends Component<{}, componentState> {
     this.state = {
       quoteText: '',
       quoteAuthor: '',
-      currentColor: ''
+      currentColor: '',
+      quoteLink: ''
     };
   }
 
@@ -26,49 +38,62 @@ class App extends Component<{}, componentState> {
 
   handleClick = () => {
     this.getNewQuote();
+  };
+
+  getRandomColor() {
+    return '#' + Math.random().toString(16).slice(-6);
   }
+
+  okRespone = (response: AxiosResponse<QuoteContent>) =>
+    this.setState({
+      quoteText: response.data.quoteText || errorMessage,
+      quoteAuthor: response.data.quoteAuthor || '',
+      currentColor: this.getRandomColor(),
+      quoteLink: response.data.quoteLink
+    });
+
+  errorResponse = () =>
+    this.setState({
+      quoteText: errorMessage,
+      quoteAuthor: '',
+      currentColor: this.getRandomColor(),
+      quoteLink: ''
+    });
 
   getNewQuote() {
     axios
       .get(
         'https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&json=?'
       )
-      .then(response =>
-        this.setState({
-          quoteText: response.data.quoteText,
-          quoteAuthor: response.data.quoteAuthor,
-          currentColor: '#' + Math.random().toString(16).slice(-6)
-        })
-      )
-      .catch(error => {
-        this.setState({
-          quoteText: 'Sorry! There is no quote:( Try again',
-          quoteAuthor: '',
-          currentColor: '#' + Math.random().toString(16).slice(-6)
-        });
-      });
+      .then(this.okRespone)
+      .catch(this.errorResponse);
+  }
+
+  get linkToShare() {
+    return `https://twitter.com/intent/tweet?url=${this.state.quoteLink}`;
   }
 
   render() {
-    const { quoteText, quoteAuthor } = this.state;
+    const { quoteText, quoteAuthor, currentColor } = this.state;
+
     return (
       <div
         className="wrapper"
-        style={{ backgroundColor: this.state.currentColor }}
+        style={{ backgroundColor: currentColor }}
       >
         <div id="quote-box" className="quote-box">
           <div className="text-wrapper">
             <div
               id="text"
               className="text"
-              style={{ color: this.state.currentColor }}
+              style={{ color: currentColor }}
             >
               {quoteText}
             </div>
             <div
               id="author"
               className="author"
-              style={{ color: this.state.currentColor }}
+              style={{ color: currentColor }}
             >
               {quoteAuthor}
             </div>
@@ -77,12 +102,13 @@ class App extends Component<{}, componentState> {
             <button
               id="new-quote"
               onClick={this.handleClick}
+              style={{ backgroundColor: currentColor }}
               className="button"
             >
               NEW QUOTE
             </button>
-            <a id="tweet-quote" href="twitter.com/intent/tweet">
-              TWEET QUOTE
+            <a id="tweet-quote" href={this.linkToShare} target="_blank">
+              <div className="tweet-button" style={{ backgroundColor: currentColor }}></div>
             </a>
           </div>
         </div>
